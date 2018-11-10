8ad5b5c64b2c8a8b62ddce957cf87d7114c9f410
Stig Nygaard <StigNygaard@users.noreply.github.com>
2018-11-10 20:19:29 +0100

    Create Stigs_Gmail_Ad_Handl.user.js

---
+++ Stigs_Gmail_Ad_Handl.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name     		Stig's Gmail Ad Handlr
+// @namespace   dk.rockland.userscript.gmail.handlr
+// @description Stig's Gmail Ad Handlr highlights ads for easy recognizability
+// @author      Stig Nygaard, http://www.rockland.dk, https://www.flickr.com/photos/stignygaard/
+// @match       https://mail.google.com/mail/u/0/*
+// @version  		1
+// @run-at      document-start
+// @grant    		none
+// @noframes
+// ==/UserScript==
+
+var DEBUG = false;
+function log(s) {
+    if (DEBUG && window.console) {
+        window.console.log(s);
+    }
+}
+
+function insertStyle() {
+    if (!document.getElementById('gmailStyle')) {
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        style.id = 'gmailStyle';
+        style.innerHTML = '.aKB {background-color:#FFA; filter: sepia(90%) !important} .aKB div {opacity:0.96 !important}';      
+        document.getElementsByTagName('head')[0].appendChild(style);
+        log('gmailStyle has been ADDED');
+    } else {
+        log('gmailStyle was already present');
+    }
+}
+
+// onload...
+insertStyle();
