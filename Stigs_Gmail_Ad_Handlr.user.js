90f463a35a1eb372fc70dc5c974a6bd46ac53045
Stig Nygaard <stig@rockland.dk>
2019-11-03 08:55:45 +0100

    Adapt to site change

--- Stigs_Gmail_Ad_Handlr.user.js
+++ Stigs_Gmail_Ad_Handlr.user.js
@@ -2,9 +2,9 @@
 // @name            Stig's Gmail Ad Handlr
 // @namespace       dk.rockland.userscript.gmail.handlr
 // @description     Stig's Gmail Ad Handlr highlights ads for easy recognizability
-// @author          Stig Nygaard, http://www.rockland.dk, https://www.flickr.com/photos/stignygaard/
+// @author          Stig Nygaard, https://www.rockland.dk, https://www.flickr.com/photos/stignygaard/
 // @match           https://mail.google.com/mail/u/0/*
-// @version         3
+// @version         4
 // @run-at          document-start
 // @grant           none
 // @noframes
@@ -22,7 +22,9 @@ function insertStyle() {
         var style = document.createElement('style');
         style.type = 'text/css';
         style.id = 'gmailStyle';
-        style.innerHTML = '.aKB {background-color:#FFA; filter: sepia(90%) !important} .aKB div {opacity:0.96 !important}';
+        style.innerHTML = 'div.Cp:not(:last-child) {opacity:0.96 !important; background-color:#FFA; filter: sepia(90%) !important}';
+
+        // style.innerHTML = '.aKB {background-color:#FFA; filter: sepia(90%) !important} .aKB div {opacity:0.96 !important} div.J-N-Jz:contains("Edit subject"), div.J-J5-Ji.J-JN-M-I-Jm {background-color:#F00}'; // div#:1dt,  // data-tooltip="Type of response"
         document.getElementsByTagName('head')[0].appendChild(style);
         log('gmailStyle has been ADDED');
     } else {
