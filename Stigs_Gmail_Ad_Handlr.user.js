6cbfd8bc431b7089ba3d7afe473499fd5bc5c5b8
Stig Nygaard <stig@rockland.dk>
2019-11-04 19:27:30 +0100

    fix

--- Stigs_Gmail_Ad_Handlr.user.js
+++ Stigs_Gmail_Ad_Handlr.user.js
@@ -4,26 +4,27 @@
 // @description     Stig's Gmail Ad Handlr highlights ads for easy recognizability
 // @author          Stig Nygaard, https://www.rockland.dk, https://www.flickr.com/photos/stignygaard/
 // @match           https://mail.google.com/mail/u/0/*
-// @version         4
+// @version         5
 // @run-at          document-start
 // @grant           none
 // @noframes
 // ==/UserScript==
 
-var DEBUG = false;
+let DEBUG = false;
 function log(s) {
     if (DEBUG && window.console) {
         window.console.log(s);
     }
 }
 
+let selector = 'div[role="tabpanel"] div.Cp:not(:last-child)';
+
 function insertStyle() {
     if (!document.getElementById('gmailStyle')) {
         var style = document.createElement('style');
         style.type = 'text/css';
         style.id = 'gmailStyle';
-        style.innerHTML = 'div.Cp:not(:last-child) {opacity:0.96 !important; background-color:#FFA; filter: sepia(90%) !important}';
-
+        style.innerHTML = selector + ' {opacity:0.96 !important; background-color:#FFA; filter: sepia(90%) !important}';
         // style.innerHTML = '.aKB {background-color:#FFA; filter: sepia(90%) !important} .aKB div {opacity:0.96 !important} div.J-N-Jz:contains("Edit subject"), div.J-J5-Ji.J-JN-M-I-Jm {background-color:#F00}'; // div#:1dt,  // data-tooltip="Type of response"
         document.getElementsByTagName('head')[0].appendChild(style);
         log('gmailStyle has been ADDED');
@@ -32,7 +33,16 @@ function insertStyle() {
     }
 }
 
+function blocker() {
+    let elems = document.querySelectorAll(selector);
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].removeEventListener('click');
+        // elems[i].parent.addEventListener('click', function(e){alert('hej');e.stopPropagation()}, true);
+    }
+}
+
 // Try immediately
 insertStyle();
 // Try again shortly after
 window.addEventListener('load', insertStyle, false);
+// window.addEventListener('load', blocker, false);
